a04a172d05b77544150e70194817de996a6d6337
Biyoni Pietersz <pieterszharsh@gmail.com>
2025-07-31 09:27:14 +0530

    Update gradio_example.py

--- code_examples/gradio_example.py
+++ code_examples/gradio_example.py
@@ -1,1 +1,13 @@
+import gradio as gr
+from transformers import pipeline
 
+# Initialize pipeline
+classifier = pipeline("sentiment-analysis")
+
+# Define prediction function
+def predict(text):
+    return classifier(text)[0]
+
+# Create Gradio interface
+interface = gr.Interface(fn=predict, inputs="text", outputs="text")
+interface.launch()
